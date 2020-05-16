4ad4d77c16da814f85c277199921d87d0ee1c20e
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-16 23:54:22 +0100

    added babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,72 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 const path = require("path");
-
-module.exports = function () {
-	return {
-		mode: "development",
-		entry: ["./src/app.js"],
-		watch: true,
-		watchOptions: {
-			aggregateTimeout: 300, // Process all changes which happened in this time into one rebuild
-			poll: 1000, // Check for changes every second,
-			ignored: /node_modules/,
-			// ignored: [
-			//   '**/*.scss', '/node_modules/'
-			// ]
-		},
-		devtool: "source-maps",
-		devServer: {
-			contentBase: path.join(__dirname, "src"),
-			watchContentBase: true,
-			hot: true,
-			open: true,
-			inline: true,
-		},
-		plugins: [
-			new HtmlWebpackPlugin({
-				title: "Webpack starter project",
-				template: path.resolve("./src/index.html"),
-			}),
-			new webpack.HotModuleReplacementPlugin(),
-		],
-		module: {
-			rules: [
-				{
-					test: /\.scss$/,
-					use: ["style-loader", "css-loader", "sass-loader"],
-				},
-				{
-					test: /\.m?js$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-						loader: "babel-loader",
-						options: {
-							presets: ["@babel/preset-env"],
-						},
-					},
-				},
-				{
-					test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
-					use: [
-						{
-							loader: "file-loader",
-							options: {
-								outputPath: "./images",
-								name: "[name].[ext]",
-							},
-						},
-					],
-				},
-				{
-					test: /\.html$/,
-					use: {
-						loader: "html-loader",
-					},
-				},
-			],
-		},
-	};
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveAppPath = (relativePath) =>
+  path.resolve(appDirectory, relativePath);
+module.exports = {
+  mode: "development",
+  entry: ['babel-polyfill',resolveAppPath("src")],
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300, // Process all changes which happened in this time into one rebuild
+    poll: 1000, // Check for changes every second,
+    ignored: /node_modules/,
+  },
+  output: {
+    filename: "static/js/bundle.[hash].js",
+  },
+  devServer: {
+    open: true,
+    contentBase: resolveAppPath("public"),
+    compress: true,
+    hot: true,
+    host: "localhost",
+    port: 8080,
+    publicPath: "/",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: resolveAppPath("public/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./images",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+        },
+      },
+    ],
+  },
 };
