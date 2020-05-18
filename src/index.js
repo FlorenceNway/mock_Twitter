5210d6448f5e361d9c71da9c54a0205446c3767c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-18 06:15:00 +0100

    import login page

--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ import './images/polygon5.png'
 import './images/polygon6.png'
 import './images/sitting4.png'
 import API from './API'
+import {loginBtn} from './login'
+import Login from './login'
 
 API.getTweets()
+
+
+loginBtn.addEventListener('click', () => {
+    Login.userLogin()
+})
