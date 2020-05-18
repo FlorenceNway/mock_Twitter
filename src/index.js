d23e5652816e9587151422c08d2628b5c70d94e6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-18 05:56:17 +0100

    import images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style/style.scss'
+import './images/polygon1.png'
 import './images/polygon3.png'
+import './images/polygon5.png'
 import './images/polygon6.png'
 import './images/sitting4.png'
 import API from './API'
