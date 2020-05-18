55693083a645173244955e67089bcf2250032d3c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-18 23:54:23 +0100

    moved folder

--- src/API.js
+++
@@ -1,9 +0,0 @@
-const API_ENDPOINT = "http://localhost:3000"
-const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`
-const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`
-
-const getTweets = async () => await fetch(TWEETS_URL).then(res => res.json()).then(response => console.log(response))
-
-export default {
-    getTweets
-}
