3bb9d4157febe3baca98d7749b33dc22c681f373
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-19 17:45:59 +0100

    fetch users

--- src/js/API.js
+++ src/js/API.js
@@ -2,8 +2,9 @@ const API_ENDPOINT = "http://localhost:3000"
 const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`
 const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`
 
-const getTweets = async () => await fetch(TWEETS_URL).then(res => res.json()).then(response => console.log(response))
+const getTweets = async () => await fetch(TWEETS_URL).then(res => res.json())
+const getUsers = async () => await fetch(USERS_URL).then(res => res.json())
 
 export default {
-    getTweets
+    getTweets,getUsers
 }
