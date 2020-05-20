232e5c06da2ef9644caf0a75900d704e2c40fb00
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-20 07:07:47 +0100

    handle user input

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,26 @@
 const userLogin = (users) => {
+    const form = document.querySelector('form')
+    const errorNode = document.createElement('p')
+
     const input = document.querySelector('input')
-    console.log(users)
-    const user = users.filter(user => user.name === input.value)
-    return user
+    if(input.value) {
+        const user = users.filter(user => user.name === input.value)
+
+        if(user.length) {
+            const errNode = form.querySelector('.wrongUsername')
+            if(errNode) { errNode.remove() }
+            
+            return user   
+        }else {
+            errorNode.innerText = 'Incorrect Username'
+            errorNode.className = 'wrongUsername'
+            form.appendChild(errorNode)
+        } 
+        return user
+
+    } else {
+        alert('Plese enter Username and Password')
+    }
 }
 
 export default {
