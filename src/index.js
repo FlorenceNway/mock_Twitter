c0e46169afa81abfa176576f379a957ec79d5b72
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-20 05:55:56 +0100

    import tweets page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import './style/style.scss'
+import Render from './js/render'
 import API from './js/API'
 import Login from './js/login'
-import Render from './js/render'
+import Tweets from './js/tweets'
 
-API.getTweets()
+
 Render.renderLoginPage()
 
-Login.loginBtn.addEventListener('click', async (e) => {
-    e.preventDefault();
 
+const loginBtn = document.querySelector('.loginBtn')
+loginBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+   
     const users = await API.getUsers()
-    Login.userLogin(users)
+    const user = Login.userLogin(users)
+    
+    const allUser_Tweets = await API.getTweets()
+    const user_Tweets = Tweets.tweets(allUser_Tweets,user[0].id)
+    //console.log(user_Tweets)
+
+    //Render.renderHomepage(user, tweets)
 })
