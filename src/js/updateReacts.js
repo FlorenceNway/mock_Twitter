2ed4ade69a1ff83859c2b287dd8e14099193bcfa
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-25 03:21:10 +0100

    update React functioons

---
+++ src/js/updateReacts.js
@@ -0,0 +1,28 @@
+import API from './API.js';
+import filledHeart from '../images/filledHeart.svg'
+import colorRetweet from '../images/colorRetweet.svg'
+
+const react_click = (react) => {
+    const reactImg = react.previousSibling
+    reactImg.addEventListener('click',(e) => {  
+        react.textContent = parseInt(react.textContent) + 1
+
+        const action = reactImg.alt
+        if(action ==='likes') {
+            changeColorAfterClick(react,filledHeart)
+            API.patchLikes(e.target.id,{likes:react.textContent})
+            
+        }else if(action === 'retweets') {
+            changeColorAfterClick(react,colorRetweet)
+            API.patchLikes(e.target.id,{retweets:react.textContent})
+        }
+    })
+} 
+
+const changeColorAfterClick = (react,svg) => {
+    react.previousSibling.src = svg
+}
+
+export default {
+    react_click
+}
