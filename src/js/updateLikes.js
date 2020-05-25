e517850a8afdfd0032c92a71c574648fc1f7cded
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-25 02:27:40 +0100

    separate function for change Likeicon color

--- src/js/updateLikes.js
+++ src/js/updateLikes.js
@@ -1,11 +1,20 @@
-import API from './API.js'
+import API from './API.js';
+import filledHeart from '../images/filledHeart.svg'
 
 const like_click = (like_Btn) => {
-    like_Btn.addEventListener('click',(e) => {  
-        like_Btn.innerText = parseInt(like_Btn.innerText) + 1
-        API.patchLikes(e.target.id,like_Btn.innerText)
+    const heartBtn = like_Btn.previousSibling
+    heartBtn.addEventListener('click',(e) => {  
+        like_Btn.textContent = parseInt(like_Btn.textContent) + 1
+        //heartBtn.src = filledHeart
+        changeColorAfterClick(like_Btn,filledHeart)
+
+        API.patchLikes(e.target.id,like_Btn.textContent)
     })
 } 
+
+const changeColorAfterClick = (react,svg) => {
+    react.previousSibling.src = svg
+}
 
 export default {
     like_click
