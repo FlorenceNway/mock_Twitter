910dcbd6a8553ba92826d82d5046e3f972e12eb6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-25 14:36:00 +0100

    comment box rendering

--- src/js/render.js
+++ src/js/render.js
@@ -6,6 +6,7 @@ import sitting from '../images/sitting4.png'
 import heart from '../images/heart.svg'
 import comment from '../images/comment.svg'
 import retweet from '../images/retweet.svg'
+import backArrow from '../images/backarrow.svg'
 
 const content = document.querySelector('.content')
 
@@ -65,12 +66,19 @@ const renderTweetpage = (user,tweets) => {
                                             <p><img src=${heart} alt='likes' id=${tweet.id}><span class='like_Btn'>${tweet.likes}</span></p>
                                             <p><img src=${retweet} alt='retweets' id=${tweet.id}><span class='retweet_Btn'>${tweet.retweets}</span></p>
                                             <p><img src=${comment} alt='comments'id=${tweet.id}><span class='comment_Btn'>${tweet.comments.length}</span></p>
-                                        </div></div>` 
+                                        </div>
+                                        <div class='commentBox'>
+                                                <textarea class='commentTextarea' placeholder='Your Comment'></textarea>
+                                                <div class='tweetBtnArea'>
+                                                    <img src=${backArrow} alt="backArrow">
+                                                    <button class='replyBtn'>Reply</button>
+                                                </div>
+                                        </div>
+                                    </div>` 
         })
         content.appendChild(userInfo)
         content.appendChild(tweetsData);
 }
-
 
 export default {
     renderLoginPage, renderTweetpage
