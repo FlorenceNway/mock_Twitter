8cf75af32be25a135b2cba2cb9025e75feb68e14
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-26 23:44:58 +0100

    API call after click on tweet

--- src/js/API.js
+++ src/js/API.js
@@ -57,7 +57,7 @@ const postComment = async(userId,tweetId,newComment) => {
     .catch(error => error)
 }
 
-const getSubTweet = async() => await fetch(TWEETS_URL).then(res => res.json())
+const getSubTweet = async() => await fetch(`${API_ENDPOINT}/tweets/1?_embed=comments`).then(res => res.json())
 
 export default {
     getTweets, getUsers, patchReact, postComment
