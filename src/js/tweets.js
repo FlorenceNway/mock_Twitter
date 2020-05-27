0f14f5a55d9053e466196b8f9be640b210b0e919
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-27 03:07:43 +0100

    unused file

--- src/js/tweets.js
+++ src/js/tweets.js
@@ -1,3 +1,6 @@
+//..........NOT USING IT ANYMORE............
+//..........NOT USING IT ANYMORE............
+
 const get_userTweets = (alltweets,userId) => {
     
     const userTweets = alltweets.filter(tweet => tweet.userId === userId)
