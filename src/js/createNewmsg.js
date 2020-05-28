2111466ba213b4ac3b86658fb354016a2aedac80
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-29 00:29:59 +0100

    create new msg btn to work

---
+++ src/js/createNewmsg.js
@@ -0,0 +1,31 @@
+import UpdateComment from './updateComment'
+
+const clickCreateNewmsg = (tweet) => {
+    const newMsgBtn = document.querySelector('.messageBtn')
+    console.log(tweet)
+    newMsgBtn.addEventListener('click', () => {
+        const commentBox = UpdateComment.renderQuickComment()
+        const siblingnode = document.querySelector('.bodyText')
+        const hasSibiling = siblingnode.nextElementSibling
+
+        if(hasSibiling === null) {
+            UpdateComment.insertAfter(siblingnode,commentBox) //insert new commentBox after bodyText div (condition control for keep adding reply box)
+        }
+
+        const TweetCommentBtn = document.querySelector('.replyBtn')
+        TweetCommentBtn.innerText = 'Tweet'
+        const numOfcomments = document.querySelector('.comment_Btn').innerText
+        createNewMsg(commentBox, tweet.id, tweet.userId, numOfcomments, TweetCommentBtn)
+    })
+    
+}
+
+const createNewMsg = (commentBox,TweetId,UserId,numOfcomments,Btn) => {
+    console.log(numOfcomments, typeof numOfcomments)
+    UpdateComment.submitComment(commentBox,TweetId,UserId,numOfcomments,Btn)
+    UpdateComment.backward()
+}
+
+export default {
+    clickCreateNewmsg
+}
