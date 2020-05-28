38ade11034d6d844d9bbb1b8df271947c28b73aa
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-28 13:58:18 +0100

    make global for date and post tweet

--- src/js/API.js
+++ src/js/API.js
@@ -3,8 +3,14 @@ const USERS_URL = `${API_ENDPOINT}/users?_embed=tweets`
 const TWEETS_URL = `${API_ENDPOINT}/tweets/?_embed=comments`
 //const TWEETS_URL = `${API_ENDPOINT}/tweets?_expand=user&_embed=comments`
 
+const d = new Date()
+const yy = d.getFullYear()
+const mm = d.getMonth() + 1
+const dd = d.getDate()
+
 const getTweets = async () => await fetch(TWEETS_URL).then(res => res.json())
 const getUsers = async () => await fetch(USERS_URL).then(res => res.json())
+const getSubTweet = async(tweetId) => await fetch(`${API_ENDPOINT}/tweets/${tweetId}?_expand=user&_embed=comments`).then(res => res.json())
 
 const patchReact = async (id,newReacts) => {
     const configObject = {
@@ -26,13 +32,7 @@ const patchReact = async (id,newReacts) => {
     .catch(error =>  error )
 }
 
-
 const postComment = async(userId,tweetId,newComment) => {
-    const d = new Date()
-    const yy = d.getFullYear()
-    const mm = d.getMonth() + 1
-    const dd = d.getDate()
-
     const configObject = {
         method: 'POST',
         headers: {
@@ -51,14 +51,39 @@ const postComment = async(userId,tweetId,newComment) => {
         if(response.ok) {
             return response.json()
         }else {
-            throw "Couldn't post the comments!"
+            throw "Couldn't post the comment!"
         }
     })
     .catch(error => error)
 }
 
-const getSubTweet = async(tweetId) => await fetch(`${API_ENDPOINT}/tweets/${tweetId}?_expand=user&_embed=comments`).then(res => res.json())
+const postTweet = async(userId,newTweet) => {
+    const configObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            userId: userId,
+            content: newTweet,
+            likes: 0,
+            retweets: 0,
+            date:`${dd}/${mm}/${yy}`
+        })
+    }
+
+    return await fetch(`${API_ENDPOINT}/tweets`, configObject)
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }else {
+            throw "Couldn't post the tweet!"
+        }
+    })
+    .catch(error => error)
+}
+
 
 export default {
-    getTweets, getUsers, patchReact, postComment, getSubTweet
+    getTweets, getUsers, patchReact, postComment, getSubTweet, postTweet
 }
