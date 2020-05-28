f1c59ccf96a4dcd6becd3764b847f92ca4c3cbb9
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-28 14:37:24 +0100

    new file created for GoToTweets

---
+++ src/js/goBackToTweets.js
@@ -0,0 +1,30 @@
+import API from './API'
+import Render from './render'
+import updateReacts from './updateReacts'
+import updateComment from './updateComment'
+
+
+const goToTweets = (button) => {
+    const user = JSON.parse(localStorage.getItem('user'))
+
+    button.addEventListener('click', async () =>{
+        const users = await API.getUsers()
+        const allTweets = await API.getTweets()
+    
+        Render.renderTweetpage(user,users,allTweets)
+        
+       const likes = document.querySelectorAll('.like_Btn');
+       [...likes].forEach(updateReacts.react_click)
+
+       const retweets = document.querySelectorAll('.retweet_Btn');
+       [...retweets].forEach(updateReacts.react_click)
+
+       const comments = document.querySelectorAll('.comment_Btn');
+       [...comments].forEach(updateComment.comment_click)
+
+    })
+}
+
+export default {
+    goToTweets
+}
