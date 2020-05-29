e59ef51bcb44c3935b6a0e1ee92a0a977572f36d
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-29 01:30:10 +0100

    get log the latest comments list

--- src/js/createNewmsg.js
+++ src/js/createNewmsg.js
@@ -1,9 +1,13 @@
 import UpdateComment from './updateComment'
+import EachTweetDetails from './eachTweetDetails'
+import API from './API'
+
+const content = document.querySelector('.content')
 
 const clickCreateNewmsg = (tweet) => {
     const newMsgBtn = document.querySelector('.messageBtn')
-    console.log(tweet)
-    newMsgBtn.addEventListener('click', () => {
+   
+    newMsgBtn.addEventListener('click', async () => {
         const commentBox = UpdateComment.renderQuickComment()
         const siblingnode = document.querySelector('.bodyText')
         const hasSibiling = siblingnode.nextElementSibling
@@ -14,11 +18,31 @@ const clickCreateNewmsg = (tweet) => {
 
         const TweetCommentBtn = document.querySelector('.replyBtn')
         TweetCommentBtn.innerText = 'Tweet'
+
         const numOfcomments = document.querySelector('.comment_Btn')
+
         createNewMsg(commentBox, tweet.id, tweet.userId, numOfcomments, TweetCommentBtn)
+
+        TweetCommentBtn.addEventListener('click', () => {
+           renderUpdatedComment(tweet)
+        })
+        
+        
+        const pop = []
+        pop[0] = tweet.comments.pop()
+        //console.log(pop)
+        //const comments = await EachTweetDetails.renderComment(pop)
+       // content.appendChild(comments)
     })
-    
 }
+
+const renderUpdatedComment = (tweet) => {
+    setTimeout(async()=> {
+        const tweetsC = await API.getSubTweet(tweet.id)
+        console.log('getSubTweet',tweetsC)
+
+    },2000)
+}   
 
 const createNewMsg = (commentBox,TweetId,UserId,numOfcomments,Btn) => {
     UpdateComment.submitComment(commentBox,TweetId,UserId,numOfcomments,Btn)
