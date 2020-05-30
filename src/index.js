49d64ade4bd7f38899dd48189f2705a40f56216f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-30 06:43:44 +0100

    formatted doc

--- src/index.js
+++ src/index.js
@@ -1,44 +1,36 @@
-import './style/style.scss'
-import Render from './js/render'
-import API from './js/API'
-import Login from './js/login'
-import UploadFile from './js/uploadProfile_pic'
-import updateReacts from './js/updateReacts'
-import updateComment from './js/updateComment'
+import "./style/style.scss";
+import Render from "./js/render";
+import API from "./js/API";
+import Login from "./js/login";
+import UploadFile from "./js/uploadProfile_pic";
+import updateReacts from "./js/updateReacts";
+import updateComment from "./js/updateComment";
 
-Render.renderLoginPage()
+Render.renderLoginPage();
 
-const loginBtn = document.querySelector('.loginBtn')
+const loginBtn = document.querySelector(".loginBtn");
 
-loginBtn.addEventListener('click', async (e) => {
-    e.preventDefault();
-   
-    const users = await API.getUsers()
-    const user = Login.userLogin(users)
-    
-    if(user) {
-        const allUser_Tweets = await API.getTweets()
-        //const user_Tweets = Tweets.get_userTweets(allUser_Tweets,user[0].id)
-       
-        Render.renderTweetpage(user,users,allUser_Tweets);
-       
-       const file_input = document.querySelector('.choose_file');
-       UploadFile.upload_profile_pic(file_input);
+loginBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
 
-       const likes = document.querySelectorAll('.like_Btn');
-       [...likes].forEach(updateReacts.react_click)
+  const users = await API.getUsers();
+  const user = Login.userLogin(users);
 
-       const retweets = document.querySelectorAll('.retweet_Btn');
-       [...retweets].forEach(updateReacts.react_click)
+  if (user) {
+    const allUser_Tweets = await API.getTweets();
 
-       const comments = document.querySelectorAll('.comment_Btn');
-       [...comments].forEach(updateComment.comment_click)
+    Render.renderTweetpage(user, users, allUser_Tweets);
 
-    //    const tweets = document.querySelectorAll('.tweetContent');
-    //    [...tweets].forEach(otherUserTweet.clickTweet)
-    }
-})
+    const file_input = document.querySelector(".choose_file");
+    UploadFile.upload_profile_pic(file_input);
 
+    const likes = document.querySelectorAll(".like_Btn");
+    [...likes].forEach(updateReacts.react_click);
 
+    const retweets = document.querySelectorAll(".retweet_Btn");
+    [...retweets].forEach(updateReacts.react_click);
 
-
+    const comments = document.querySelectorAll(".comment_Btn");
+    [...comments].forEach(updateComment.comment_click);
+  }
+});
